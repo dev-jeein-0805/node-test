c152784731de50666655daa988338e487aecadef
Jeein Sophie Han <jeein.han91@gmail.com>
2024-12-07 18:12:00 +0900

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("Hello noddeee");
+console.log("1. 물 가져다 주기");
 
-console.log('test22');
+setTimeout(() => {
+    console.log("2. 햄버거 주기");
+}, 3000);
+
+setTimeout(() => {
+    console.log("3. 감자튀김 주기");
+}, 2000);
+
+console.log("4. 콜라 가져다 주기");
+
+// node index.js 실행
+// 1. 물 가져다 주기
+// 4. 콜라 가져다 주기
+// 3. 감자튀김 주기
+// 2. 햄버거 주기
